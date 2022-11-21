f5f726f29829deb12b7c2136c7ce6f20bb8fe107
LocCuTo <104060740+LocCuTo@users.noreply.github.com>
2022-11-21 21:19:32 +0700

    Add files via upload

---
+++ src/Components/Game.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Board from './Board';
+import '../styles/gameStyle.css';
+import { calculateWinner } from '../utils';
+
+const Game = () => {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const winner = calculateWinner(board);
+
+    const handleClick = (index) => {
+        const boardCopy = [...board];
+        if (winner || boardCopy[index]) return;
+        boardCopy[index] = xIsNext ? 'X' : 'O';
+        setBoard(boardCopy);
+        setXIsNext(!xIsNext);
+    };
+
+    const handleResetGame = () => {
+        setBoard(Array(9).fill(null));
+        setXIsNext(true);
+    };
+
+    return (
+        <div>
+            <Board cells={board} onClick={handleClick}></Board>
+            <div className="game-winner">{winner ? `Winner is ${winner}` : ''}</div>
+            <button className="game-reset" onClick={handleResetGame}>
+                Reset Game
+            </button>
+        </div>
+    );
+};
+
+export default Game;
